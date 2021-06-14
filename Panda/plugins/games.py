779aafe9c8fa7a50cc46e166725072b2a2fdf1be
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-06-14 22:48:48 +0700

    Create games.py

---
+++ Panda/plugins/games.py
@@ -0,0 +1,25 @@
+import asyncio
+from telethon import events
+from . import pandaub, edit_or_reply
+
+plugin_category = "useless"
+
+
+@pandaub.panda_cmd(
+    pattern="xoagame$",
+    command=("xoagame", plugin_category),
+    info={
+        "header": "Game busuk",
+        "usage": "{tr}xoagame",
+    },
+)
+async def gamez(event):
+    if event.fwd_from:
+        return
+    botusername = "@xobot"
+    noob = "play"
+    if event.reply_to_msg_id:
+        await event.get_reply_message()
+    tap = await bot.inline_query(botusername, noob)
+    await tap[0].click(event.chat_id)
+    await event.delete()
