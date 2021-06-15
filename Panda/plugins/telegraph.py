0cb27e1caf5f8f9f053fb7368bb2586fca5fdfe2
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-06-15 10:34:31 +0700

    Update telegraph.py

--- Panda/plugins/telegraph.py
+++ Panda/plugins/telegraph.py
@@ -12,8 +12,13 @@ plugin_category = "misc"
 
 
 telegraph = Telegraph()
-r = telegraph.create_account(short_name="telegraph")
+r = telegraph.create_account(short_name=Config.TELEGRAPH_SHORT_NAME)
 auth_url = r["auth_url"]
+
+
+def resize_image(image):
+    im = Image.open(image)
+    im.save(image, "PNG")
 
 
 @pandaub.ilhammansiz_cmd(
@@ -34,82 +39,70 @@ auth_url = r["auth_url"]
         ],
     },
 )  # sourcery no-metrics
-async def telegraphs(graph):
-    await graph.edit("`Sedang Memproses... Sabar ya bentar juga muncul😁`")
-    if not graph.text[0].isalpha() and graph.text[0] not in ("/", "#", "@", "!"):
-        if graph.fwd_from:
-            return
-        if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
-            os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-        if graph.reply_to_msg_id:
-            start = datetime.now()
-            r_message = await graph.get_reply_message()
-            input_str = graph.pattern_match.group(1)
-            if input_str == "m":
-                downloaded_file_name = await bot.download_media(
-                    r_message, Config.TMP_DOWNLOAD_DIRECTORY
-                )
-                end = datetime.now()
-                ms = (end - start).seconds
-                await graph.edit(
-                    "Di Download Ke {} Dalam {} Detik.".format(downloaded_file_name, ms)
-                )
-                try:
-                    if downloaded_file_name.endswith((".webp")):
-                        resize_image(downloaded_file_name)
-                except AttributeError:
-                    return await graph.edit("`Tidak Ada Media Yang Disediakan`")
-                try:
-                    start = datetime.now()
-                    media_urls = upload_file(downloaded_file_name)
-                except exceptions.TelegraphException as exc:
-                    await graph.edit("ERROR: " + str(exc))
-                    os.remove(downloaded_file_name)
-                else:
-                    end = datetime.now()
-                    ms_two = (end - start).seconds
-                    os.remove(downloaded_file_name)
-                    await graph.edit(
-                        "Berhasil Mengunggah Ke [Telegraph](https://telegra.ph{}).".format(
-                            media_urls[0], (ms + ms_two)
-                        ),
-                        link_preview=True,
-                    )
-            elif input_str == "t":
-                user_object = await bot.get_entity(r_message.from_id)
-                title_of_page = user_object.first_name  # + " " + user_object.last_name
-                # apparently, all Users do not have last_name field
-                page_content = r_message.message
-                if r_message.media:
-                    if page_content != "":
-                        title_of_page = page_content
-                    downloaded_file_name = await bot.download_media(
-                        r_message, TEMP_DOWNLOAD_DIRECTORY
-                    )
-                    m_list = None
-                    with open(downloaded_file_name, "rb") as fd:
-                        m_list = fd.readlines()
-                    for m in m_list:
-                        page_content += m.decode("UTF-8") + "\n"
-                    os.remove(downloaded_file_name)
-                page_content = page_content.replace("\n", "<br>")
-                response = telegraph.create_page(
-                    title_of_page, html_content=page_content
-                )
-                end = datetime.now()
-                ms = (end - start).seconds
-                await graph.edit(
-                    "Berhasil Mengunggah Ke [Telegraph](https://telegra.ph/{}).".format(
-                        response["path"], ms
-                    ),
-                    link_preview=True,
-                )
+async def _(event):
+    "To get telegraph link."
+    pandaevent = await edit_or_reply(event, "`processing........`")
+    if BOTLOG:
+        await event.client.send_message(
+            BOTLOG_CHATID,
+            f"Created New Telegraph account {auth_url} for the current session. \n**Do not give this url to anyone, even if they say they are from Telegram!**",
+        )
+    optional_title = event.pattern_match.group(5)
+    if not event.reply_to_msg_id:
+        return await pandaevent.edit(
+            "`Reply to a message to get a permanent telegra.ph link.`",
+        )
+
+    start = datetime.now()
+    r_message = await event.get_reply_message()
+    input_str = (event.pattern_match.group(4)).strip()
+    if input_str in ["media", "m"]:
+        downloaded_file_name = await event.client.download_media(
+            r_message, Config.TEMP_DIR
+        )
+        await pandaevent.edit(f"`Downloaded to {downloaded_file_name}`")
+        if downloaded_file_name.endswith((".webp")):
+            resize_image(downloaded_file_name)
+        try:
+            media_urls = upload_file(downloaded_file_name)
+        except exceptions.TelegraphException as exc:
+            await pandaevent.edit(f"**Error : **\n`{str(exc)}`")
+            os.remove(downloaded_file_name)
         else:
-            await graph.edit(
-                "`Mohon Balas Ke Pesan, Untuk Mendapatkan Link Telegraph Permanen.`"
+            end = datetime.now()
+            ms = (end - start).seconds
+            os.remove(downloaded_file_name)
+            await pandaevent.edit(
+                f"**link : **[telegraph](https://telegra.ph{media_urls[0]})\
+                    \n**Time Taken : **`{ms} seconds.`",
+                link_preview=True,
             )
-
-
-def resize_image(image):
-    im = Image.open(image)
-    im.save(image, "PNG")
+    elif input_str in ["text", "t"]:
+        user_object = await event.client.get_entity(r_message.sender_id)
+        title_of_page = user_object.first_name  # + " " + user_object.last_name
+        # apparently, all Users do not have last_name field
+        if optional_title:
+            title_of_page = optional_title
+        page_content = r_message.message
+        if r_message.media:
+            if page_content != "":
+                title_of_page = page_content
+            downloaded_file_name = await event.client.download_media(
+                r_message, Config.TEMP_DIR
+            )
+            m_list = None
+            with open(downloaded_file_name, "rb") as fd:
+                m_list = fd.readlines()
+            for m in m_list:
+                page_content += m.decode("UTF-8") + "\n"
+            os.remove(downloaded_file_name)
+        page_content = page_content.replace("\n", "<br>")
+        response = telegraph.create_page(title_of_page, html_content=page_content)
+        end = datetime.now()
+        ms = (end - start).seconds
+        panda = f"https://telegra.ph/{response['path']}"
+        await pandaevent.edit(
+            f"**link : ** [telegraph]({cat})\
+                 \n**Time Taken : **`{ms} seconds.`",
+            link_preview=True,
+        )
